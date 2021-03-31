5087fe6553125eebb3d9c1d63aa8984a613d6537
gizem <gizemates@iyte.edu.tr>
2021-04-01 00:29:33 +0200

    elapsed time calculated based on python time library

--- logging/data_logger_node.py
+++ logging/data_logger_node.py
@@ -4,7 +4,7 @@
 		TODO: make it as gui as you did before.
 """
 
-import rospy
+import rospy, time
 import Data.data_logger_module_v2 as data_logger
 from geometry_msgs.msg import Pose
 from std_msgs.msg import Int8
@@ -28,14 +28,17 @@ if __name__ == "__main__":
 	global test_pose, test_gain
 	try:
 		rospy.init_node('data_logger_node')
-		start_time = rospy.get_time()
+		# start_time = rospy.get_time()
+		start_time = time.time()
+		current_time = rospy.get_time()
 		data_logger.enable_logging()
 		sub_testpose = rospy.Subscriber('/Tee_goal_pose', Pose, callback_pose)
 		sub_testgain = rospy.Subscriber('/selector', Int8, callback_gain)
 		rate = rospy.Rate(10)
 		while not rospy.is_shutdown():
+			elapsed_time = time.time() - start_time
+			# elapsed_time = current_time - start_time
 			current_time = rospy.get_time()
-			elapsed_time = start_time - current_time
 			data_logger.log_metrics(elapsed_time, test_pose, test_gain)
 			rate.sleep()
 	except KeyboardInterrupt:
