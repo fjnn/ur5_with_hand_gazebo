da55b7a08e21410691987f37b884b5b5aaf7b0e4
gizem <gizemates@iyte.edu.tr>
2021-04-01 02:18:54 +0200

    data module updated for actual purpose. Ready

--- logging/Data/data.py
+++ logging/Data/data.py
@@ -18,7 +18,7 @@ from datetime import datetime
 
 # OUTPUT_FOLDER = "../Output"  # take it as a rosparam
 OUTPUT_FOLDER = "/home/gizem/catkin_ws/src/ur5_with_hand_gazebo/logging/Output"  # take it as a rosparam
-OUTPUT_FILENAME_PREFIX = "gizem_test"
+OUTPUT_FILENAME_PREFIX = "JSM_gizem_test"
 
 
 ID_ELAPSED_TIME = 'elapsed_time'
@@ -29,12 +29,25 @@ ID_TEE_ORI_X = 'tool_ori_x'
 ID_TEE_ORI_Y = 'tool_ori_y'
 ID_TEE_ORI_Z = 'tool_ori_z'
 ID_TEE_ORI_W = 'tool_ori_w'
-# ID_PITCH = 'human_pitch'
-# ID_ROLL = 'human_roll'
-# ID_YAW = 'human_yaw'
-ID_GAIN = 'adaptive_gain'
+ID_WRIST_ANGLE_X = 'wrist_x'
+ID_WRIST_ANGLE_Y = 'wrist_y'
+ID_WRIST_ANGLE_Z = 'wrist_z'
+ID_HAND_POS_X = 'hand_pos_x'
+ID_HAND_POS_Y = 'hand_pos_y'
+ID_HAND_POS_Z = 'hand_pos_z'
+ID_HAND_ORI_X = 'hand_ori_x'
+ID_HAND_ORI_Y = 'hand_ori_y'
+ID_HAND_ORI_Z = 'hand_ori_z'
+ID_HAND_ORI_W = 'hand_ori_w'
+ID_GAIN_X = 'gain_x'
+ID_GAIN_Y = 'gain_y'
+ID_GAIN_Z = 'gain_z'
 
-DATA_LABELS = (ID_ELAPSED_TIME, ID_TEE_POS_X, ID_TEE_POS_Y, ID_TEE_POS_Z, ID_TEE_ORI_X, ID_TEE_ORI_Y, ID_TEE_ORI_Z, ID_TEE_ORI_W, ID_GAIN)
+DATA_LABELS = (ID_ELAPSED_TIME, 
+								ID_TEE_POS_X, ID_TEE_POS_Y, ID_TEE_POS_Z, ID_TEE_ORI_X, ID_TEE_ORI_Y, ID_TEE_ORI_Z, ID_TEE_ORI_W, 
+								ID_WRIST_ANGLE_X, ID_WRIST_ANGLE_Y, ID_WRIST_ANGLE_Z,
+								ID_HAND_POS_X, ID_HAND_POS_Y, ID_HAND_POS_Z, ID_HAND_ORI_X, ID_HAND_ORI_Y, ID_HAND_ORI_Z, ID_HAND_ORI_W, 
+								ID_GAIN_X, ID_GAIN_Y, ID_GAIN_Z)
 DATA_INDICES = {
 								ID_ELAPSED_TIME: 0,
 								ID_TEE_POS_X: 1,
@@ -44,7 +57,19 @@ DATA_INDICES = {
 								ID_TEE_ORI_Y: 5,
 								ID_TEE_ORI_Z: 6,
 								ID_TEE_ORI_W: 7,
-								ID_GAIN: 8
+								ID_WRIST_ANGLE_X: 8,
+								ID_WRIST_ANGLE_Y: 9,
+								ID_WRIST_ANGLE_Z: 10,
+								ID_HAND_POS_X: 11,
+								ID_HAND_POS_Y: 12,
+								ID_HAND_POS_Z: 13,
+								ID_HAND_ORI_X: 14,
+								ID_HAND_ORI_Y: 15,
+								ID_HAND_ORI_Z: 16,
+								ID_HAND_ORI_W: 17,
+								ID_GAIN_X: 18,
+								ID_GAIN_Y: 19,
+								ID_GAIN_Z: 20
 								}
 
 
