c642433efe3a2459cc28332eb664e5d6ef143bd6
gizem <gizemates@iyte.edu.tr>
2021-04-02 22:22:34 +0200

    Imu node /wrist_angle and /gyro_wrist updated

--- src/Classes/IMU_class_elbow_angle.py
+++ src/Classes/IMU_class_elbow_angle.py
@@ -51,15 +51,15 @@ class IMUsubscriber:
 
         self.gyro_elbow = Vector3()
         self.gyro_wrist = Vector3()
-	
+
         self.q_wrist_tsm_init = Quaternion(1.0, 1.0, 1.0, 1.0)
         self.q_wrist_tsm = Quaternion(0, 0, 0, 1.0)
 
         self.hand_pose = Pose()
-        self.wrist_angles = np.array([0.0, 0.0, 0.0])
+        self.wrist_angles = Vector3()
         ## this is a bit unnecessary now after wrist_angles_pose
         ## But I want to keep it for consistency to my full arm IMU class
-        self.human_joint_imu = JointState() 
+        self.human_joint_imu = JointState()
         self.human_joint_imu.name = ['left_wrist_0', 'left_wrist_1', 'left_wrist_2']
         self.human_joint_imu.position = [0.0, 0.0, 0.0]
         self.calibration_flag = 0
@@ -71,6 +71,7 @@ class IMUsubscriber:
     def init_subscribers_and_publishers(self):
         self.pub = rospy.Publisher('/human_joint_states', JointState, queue_size=1)
         self.pub_wrist_angles = rospy.Publisher('/wrist_angles', Vector3, queue_size=1)
+        self.pub_gyro_wrist = rospy.Publisher('/gyro_wrist', Vector3, queue_size=1)
         self.pub_hand_pose = rospy.Publisher('/hand_pose', Pose, queue_size=1)
 #        self.pub_test = rospy.Publisher('/my_test_msg', test_msg, queue_size=10)
 #        self.pub_p_hand = rospy.Publisher('/p_hand', Vector3, queue_size=1)
@@ -78,7 +79,7 @@ class IMUsubscriber:
         self.sub_imu_w = rospy.Subscriber('/sensor_l_wrist', Imu, self.cb_imu_wrist)
         # if self.number==4:
             # self.sub_imu_c = rospy.Subscriber('/sensor_r_wrist', Imu, self.cb_imu_chest)
-        
+
 #        self.log_start_time = rospy.get_time()
         self.runflag = True
 #        _ROSTIME_START = rospy.get_time()
@@ -94,10 +95,11 @@ class IMUsubscriber:
 					self.hand_pos_calculate()
 					self.pub.publish(self.human_joint_imu)
 					self.pub_hand_pose.publish(self.hand_pose)
-					
+
 					# Clean this part later
 					wrist_angles_pose = Vector3(self.wrist_angles[0], self.wrist_angles[1], self.wrist_angles[2])
 					self.pub_wrist_angles.publish(wrist_angles_pose)
+					self.pub_gyro_wrist.publish(self.gyro_wrist)
 					# TODO: here needs update p_hand = hand_link*q_wrist
 					# self.p_hand.x = np.degrees(self.human_joint_imu.position[6])  # pitch
 					# self.p_hand.y = np.degrees(self.human_joint_imu.position[7])  # yaw
@@ -131,7 +133,7 @@ class IMUsubscriber:
         self.human_joint_imu.position[0] = self.wrist_angles[0]  # pitch
         self.human_joint_imu.position[1] = self.wrist_angles[1]  # yaw
         self.human_joint_imu.position[2] = self.wrist_angles[2]  # roll
-        
+
     def hand_pos_calculate2(self, v=hand_link):
 			"""
 			NOTE: Use this when you calibrate hand in BENT pose
@@ -157,15 +159,15 @@ class IMUsubscriber:
 					self.tf_wrist.position.x = v_rotated[0]-v_rotated_init[0]
 					self.tf_wrist.position.y = v_rotated[1]-v_rotated_init[1]
 					self.tf_wrist.position.z = v_rotated[2]-v_rotated_init[2]
-					
+
 					self.hand_pose.position.x = v_rotated[0]
 					self.hand_pose.position.y = v_rotated[1]
 					self.hand_pose.position.z = v_rotated[2]
 					self.tf_wrist.orientation = self.q_wrist_tsm
 					# self.tf_wrist.orientation = kinematic.q_multiply(robot_init.orientation, self.q_wrist_tsm)
 					self.hand_pose.orientation = self.q_wrist_tsm
-			
-			
+
+
     def hand_pos_calculate(self, v=hand_link):
 				"""
 				NOTE: Use this when you calibrate hand in HORIZONTAL pose
@@ -180,11 +182,9 @@ class IMUsubscriber:
 					print "Cont?"
 					dummy_input = raw_input()
 					_HAND_POS_INIT = True
-				
+
 				# Calculate pose
 				self.hand_pose.position.x = v_rotated[0]
 				self.hand_pose.position.y = v_rotated[1]
 				self.hand_pose.position.z = v_rotated[2]
 				self.hand_pose.orientation = self.q_wrist_tsm
-	    
-
