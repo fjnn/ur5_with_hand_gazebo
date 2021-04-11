89584f38c19e5d38987b0ea2e07ea281b9d06963
gizem <gizemates@iyte.edu.tr>
2021-04-11 19:51:54 +0200

    imu node rate increased

--- src/imu_based_hand_pose_publisher.py
+++ src/imu_based_hand_pose_publisher.py
@@ -5,7 +5,7 @@ from Classes.IMU_class_elbow_angle import IMUsubscriber
 from geometry_msgs.msg import Vector3
 
 if __name__ == "__main__": 
-	IMU = IMUsubscriber()
+	IMU = IMUsubscriber(rate=100)
 	IMU.init_subscribers_and_publishers()
 	while not rospy.is_shutdown():
 		IMU.update()
